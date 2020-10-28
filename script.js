cd41194aad7b69f86e1691caee1e8dc1bb960f9a
lexyrebster <alexismreb@gmail.com>
2020-10-27 18:10:51 -0700

    questions, choices, answers

---
+++ script.js
@@ -0,0 +1,32 @@
+
+// variable with an array //
+var questions = [
+
+    {
+        // object //
+        title: "What does HTML stand for?",
+        choices: ["Hypertext Markup Language", "Hypersensitive Toggle Mark Language", "Nothing", "Human Texting Manual Language"],
+        answer: "Hypertext Markup Language"
+    },
+    {
+        title: "Which is NOT a primitive date type for JavaScript",
+        choices: ["String", "Array", "Number", "Boolean"],
+        answer: "Array"
+    },
+    {
+        title: "What does CSS stand for?",
+        choices: ["Copying Stylesheet Status", "Creative Styling Standard", "Cascading Style Sheets", "Cow Stands Still"],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        title: "What are the two things you need in order to run an AJAX method, with regards to APIs",
+        choices: ["URL & HTTP Method", "API Key & Endpoint", "Syntax & Styling", "Server & Path"],
+        answer: "URL & HTTP Method"
+    },
+    {
+        title: "What's NOT an example of an API (Application Programming Interface)?",
+        choices: ["Twitter Bots", "Weather Snippets", "Travel Booking", "All listed are examples of APIs"],
+        answer: "All listed are examples of APIs"
+    },
+
+];
